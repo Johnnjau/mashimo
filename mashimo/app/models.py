36f5bb34a20c35f6752314f929daeaabdf2b3e5d
John <johnnjau202@gmail.com>
2024-05-07 15:30:54 +0300

    user_edit

--- mashimo/app/models.py
+++ mashimo/app/models.py
@@ -33,8 +33,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
 
-
+    
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
